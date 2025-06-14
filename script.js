805302d958e671de6c51a0a085307bbf38b9bc0f
TVA018 <thomas.vu018@gmail.com>
2025-06-14 13:39:08 -0400

    add set button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const SET_COLUMN = document.getElementById("set-column");
 const TEAM_1_COLUMN = document.getElementById("team-1-column");
 const TEAM_2_COLUMN = document.getElementById("team-2-column");
+
