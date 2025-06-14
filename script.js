f68024f3de4ba978335ddcab6cb21f12ece5187c
TVA018 <thomas.vu018@gmail.com>
2025-06-14 13:23:46 -0400

    update

---
+++ script.js
@@ -0,0 +1,3 @@
+const SET_COLUMN = document.getElementById("set-column");
+const TEAM_1_COLUMN = document.getElementById("team-1-column");
+const TEAM_2_COLUMN = document.getElementById("team-2-column");
