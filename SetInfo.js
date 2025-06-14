72c786b8e3e5a25bb310ecebf4134107061cbe7f
TVA018 <thomas.vu018@gmail.com>
2025-06-14 17:35:20 -0400

    complete basic functionality

---
+++ SetInfo.js
@@ -0,0 +1,44 @@
+class SetInfo {
+    constructor(setContainer, team1Container, team2Container, removeSetContainer, index){
+        this.index = index;
+        this._removeCallback = () => {};
+
+        this.setLabel = document.createElement("div");
+        this.team1Points = document.createElement("input");
+        this.team2Points = document.createElement("input");
+        this.removeSetButton = document.createElement("button");
+
+        this.setLabel.className = "set-number row-height";
+        this.setLabel.innerHTML = index + 1;
+        this.team1Points.className = "points row-height";
+        this.team1Points.type = "number";
+        this.team2Points.className = "points row-height";
+        this.team2Points.type = "number";
+        this.removeSetButton.className = "remove-set-button";
+        this.removeSetButton.textContent = "-";
+        this.removeSetButton.onclick = () => {this.remove()}; // have to wrap in a lambda function so that "this" references SetInfo, not the removeSetButton
+
+        setContainer.append(this.setLabel);
+        team1Container.append(this.team1Points);
+        team2Container.append(this.team2Points);
+        removeSetContainer.append(this.removeSetButton);
+    }
+
+    setIndex(newIndex){
+        this.index = newIndex;
+        this.setLabel.innerHTML = newIndex + 1;
+    }
+
+    /** Function should be in the form func(setIndex) */
+    setOnRemoveCallback(callbackFunc){
+        this._removeCallback = callbackFunc;
+    }
+
+    remove(){
+        this.setLabel.remove();
+        this.team1Points.remove();
+        this.team2Points.remove();
+        this.removeSetButton.remove();
+        this._removeCallback(this.index);
+    }
+}
--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 const SET_COLUMN = document.getElementById("set-column");
-const ADD_SET_BUTTON = document.getElementById("add-set")
+const REMOVE_SET_COLUMN = document.getElementById("remove-set-column");
+const ADD_SET_BUTTON = document.getElementById("add-set");
 
 const TEAM_1_COLUMN = document.getElementById("team-1-column");
 const TEAM_2_COLUMN = document.getElementById("team-2-column");
 
+const SCRIM_DATE = document.getElementById("scrim-date");
+const GENERATE_LOG = document.getElementById("generate-log");
+const YOUR_TEAM = document.getElementById("your-team");
+const OPPO_TEAM = document.getElementById("opposite-team");
+const IS_COMP_CHECK = document.getElementById("scrim-type");
+
+const SETS = [];
+
+function onSetRemoval(indexOfRemovedSet){
+    SETS.splice(indexOfRemovedSet, 1);
+
+    for(let i = indexOfRemovedSet; i < SETS.length; i++){
+        SETS[i].setIndex(i);
+    }
+}
+
+SCRIM_DATE.valueAsDate = new Date();
+
+ADD_SET_BUTTON.onclick = () => {
+    let info = new SetInfo(SET_COLUMN, TEAM_1_COLUMN, TEAM_2_COLUMN, REMOVE_SET_COLUMN, SETS.length);
+    info.setOnRemoveCallback(onSetRemoval);
+    SETS.push(info);
+}
+
+GENERATE_LOG.onclick = () => {
+    let logString = `${SCRIM_DATE.value} | ${IS_COMP_CHECK.checked ? "COMP " : ""}Scrim against ${OPPO_TEAM.value}\n\nLineup goes here\n`;
+    for(let i = 0; i < SETS.length; i++){
+        const set = SETS[i];
+        const team1Points = parseInt(set.team1Points.value);
+        const team2Points = parseInt(set.team2Points.value);
+        const winningTeam = (team1Points > team2Points) ? YOUR_TEAM.value : OPPO_TEAM.value;
+        logString += `\nSet ${set.index + 1}: ${team1Points}-${team2Points} ${winningTeam}`;
+    }
+
+    navigator.clipboard.writeText(logString);
+    alert("COPIED TO CLIPBOARD");
+}
